a57cdbb63e15756f4c2e055f664b0b340e7dcd1e
KevinKal <Kevinkal513@aim.com>
2014-04-10 11:41:08 -0400

    Updated main.xml for preferences

--- src/com/fsc/TeamMikasaProject/MyActivity.java
+++ src/com/fsc/TeamMikasaProject/MyActivity.java
@@ -1,7 +1,14 @@
 package com.fsc.TeamMikasaProject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import java.util.prefs.Preferences;
 
 public class MyActivity extends Activity {
     /**
@@ -11,5 +18,24 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_action_bar, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Intent intent = new Intent(ControllerActivity.this, PreferencesActivity.class);
+                startActivity(intent);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
